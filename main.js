e357e0f286d4e95387a7b31eb92ed9e570e35abf
Hein Htet Win <heinhtetwin@ayainnovation.com>
2023-10-19 22:18:10 +0630

    roll back

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World! Testing Auto\n');
+  res.end('Hello, World!\n');
 });
 
 server.listen(3000, () => {
